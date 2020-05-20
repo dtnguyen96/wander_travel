11055a1be226b4bb9ed21513bd6b23a11db54971
Dung Nguyen <dung34nguyen@gmail.com>
2020-05-20 13:57:22 -0500

    change html string to display more weather info

--- Public/helper.js
+++ Public/helper.js
@@ -11,6 +11,8 @@ const createVenueHTML = (name, location, iconSource) => {
     console.log(currentDay)
     return `<h2>${weekDays[(new Date()).getDay()]}</h2>
           <h2>Temperature: ${kelvinToFahrenheit(currentDay.main.temp)}&deg;F</h2>
+          <h2>Feels like: ${kelvinToFahrenheit(currentDay.main.feels_like)}&deg;F</h2>
+          <h2>Humidity: ${currentDay.main.humidity}% </h2>
           <h2>Condition: ${currentDay.weather[0].description}</h2>
         <img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`;
   }
